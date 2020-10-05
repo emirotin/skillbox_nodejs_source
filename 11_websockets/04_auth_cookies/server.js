183db415b4fb1bd1169e48da820ec0b72d27901e
Eugene Mirotin <emirotin@gmail.com>
2020-10-05 19:33:29 +0300

    WS cookie auth

---
+++ 11_websockets/04_auth_cookies/server.js
@@ -0,0 +1,93 @@
+require("dotenv").config();
+
+const { nanoid } = require("nanoid");
+const express = require("express");
+const http = require("http");
+const cookie = require("cookie");
+
+const WebSocket = require("ws");
+
+const DB = {
+  users: [
+    { id: 1, username: "jane", password: "pwd007" },
+    { id: 2, username: "joe", password: "pwd008" },
+    { id: 3, username: "jack", password: "pwd009" },
+  ],
+  tokens: {},
+};
+
+const clients = new Map();
+
+const app = express();
+app.use(express.static("public"));
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ clientTracking: false, noServer: true });
+
+app.post("/login", express.json(), (req, res) => {
+  const { username, password } = req.body;
+  const user = DB.users.find((u) => u.username === username);
+  if (!user) {
+    return res.status(401).send("Unknown username");
+  }
+  if (user.password !== password) {
+    return res.status(401).send("Wrong password");
+  }
+  const token = nanoid();
+  DB.tokens[token] = user.id;
+  res.cookie("token", token).send({ ok: true });
+});
+
+server.on("upgrade", (req, socket, head) => {
+  const cookies = cookie.parse(req.headers["cookie"]);
+  const token = cookies && cookies["token"];
+  const userId = token && DB.tokens[token];
+
+  if (!userId) {
+    socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
+    socket.destroy();
+    return;
+  }
+
+  req.userId = userId;
+
+  wss.handleUpgrade(req, socket, head, (ws) => {
+    wss.emit("connection", ws, req);
+  });
+});
+
+wss.on("connection", (ws, req) => {
+  const { userId } = req;
+  const user = DB.users.find((u) => u.id === userId);
+
+  clients.set(userId, ws);
+
+  ws.on("close", () => {
+    clients.delete(userId);
+  });
+
+  ws.on("message", (message) => {
+    let data;
+    try {
+      data = JSON.parse(message);
+    } catch (err) {
+      return;
+    }
+
+    if (data.type === "chat_message") {
+      const fullMessage = JSON.stringify({
+        ...data,
+        name: user.username,
+      });
+
+      for (ws of clients.values()) {
+        ws.send(fullMessage);
+      }
+    }
+  });
+});
+
+server.listen(process.env.PORT, () => {
+  console.log(`Server listening on http://localhost:${process.env.PORT}`);
+});
